487f08ddf4f49aae32fe343d7303e81aa1c04c20
MestreLion <linux@rodrigosilva.com>
2020-11-20 02:05:54 -0300

    add CS50 repo and library installer

---
+++ install.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# Add Harvard's CS50 apt repo and install CS50 C library <libcs50.h>
+
+# https://github.com/cs50/libcs50
+# https://packagecloud.io/install/repositories/cs50/repo/script.deb.sh
+
+#------------------------------------------------------------------------------
+
+set -Eeuo pipefail
+trap 'error "line $LINENO, status $?: $BASH_COMMAND"' ERR
+
+#------------------------------------------------------------------------------
+
+gpg_key_url="https://packagecloud.io/cs50/repo/gpgkey"
+apt_config_url="https://packagecloud.io/install/repositories/cs50/repo/config_file.list?source=script"
+apt_source_path="/etc/apt/sources.list.d/cs50.list"
+
+#------------------------------------------------------------------------------
+
+os=
+dist=
+
+#------------------------------------------------------------------------------
+
+try()     { "$@" 2>/dev/null || :; }
+bold()    { try tput bold; printf '%s' "$@"; try tput sgr0; echo; }
+color()   { if (($# > 1)); then try tput setaf "$1"; shift; bold "$@"; fi; }
+red()     { color 1 "$@"; }
+green()   { color 2 "$@"; }
+yellow()  { color 3 "$@"; }
+blue()    { color 4 "$@"; }
+fatal()   { red "${0##*/}: fatal${1:+: $@}" >&2; exit 1; }
+message() { green "* $@"; }
+
+#------------------------------------------------------------------------------
+
+is_root() { [[ "$(id -u)" -eq 0 ]]; }
+require_root() { is_root || fatal "You must run this as root"; }
+
+
+detect_os () {
+	# some systems dont have lsb-release yet have the lsb_release binary and
+	# vice-versa
+	if [ -e /etc/lsb-release ]; then
+		. /etc/lsb-release
+
+		if [ "${ID:-}" = "raspbian" ]; then
+			os=${ID}
+			dist=`cut --delimiter='.' -f1 /etc/debian_version`
+		else
+			os=${DISTRIB_ID}
+			dist=${DISTRIB_CODENAME}
+			if [ -z "$dist" ]; then
+				dist=${DISTRIB_RELEASE}
+			fi
+		fi
+
+	elif [ `which lsb_release 2>/dev/null` ]; then
+		dist=`lsb_release -c | cut -f2`
+		os=`lsb_release -i | cut -f2 | awk '{ print tolower($1) }'`
+
+	elif [ -e /etc/debian_version ]; then
+		# some Debians have jessie/sid in their /etc/debian_version
+		# while others have '6.0.7'
+		os=`cat /etc/issue | head -1 | awk '{ print tolower($1) }'`
+		if grep -q '/' /etc/debian_version; then
+			dist=`cut --delimiter='/' -f1 /etc/debian_version`
+		else
+			dist=`cut --delimiter='.' -f1 /etc/debian_version`
+		fi
+	fi
+
+	# remove whitespace from OS and dist name
+	os="${os// /}"
+	dist="${dist// /}"
+
+	echo "Detected operating system as $os/$dist."
+	if [ -z "$dist" ]; then
+		echo "Your OS distro or version are not supported."
+		exit 1
+	fi
+}
+
+message "Add Harvard's CS50 apt repo and install CS50 C library <libcs50.h>"
+
+require_root
+detect_os
+
+apt_config_url+="&os=${os}&dist=${dist}"
+message "Repository URL: $apt_config_url"
+
+# create an apt config file for this repository
+message "Installing $apt_source_path"
+curl -sSf "${apt_config_url}" | tee "$apt_source_path" >/dev/null
+
+# import the gpg key
+message "Importing packagecloud gpg key"
+curl -L "${gpg_key_url}" | apt-key add -
+
+message "Updating apt package list"
+apt update
+
+message "Installing CS50 C library"
+apt install -y libcs50
