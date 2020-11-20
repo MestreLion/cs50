5037ea8bd02d7b601f5e76f58a8fed9c11a7f0c6
MestreLion <linux@rodrigosilva.com>
2020-11-20 19:29:08 -0300

    cash: refactor "foreach" loop to a simpler, more legible while() loop

--- week1 - C/cash/cash.c
+++ week1 - C/cash/cash.c
@@ -5,7 +5,7 @@
 
 #include <cs50.h>
 
-// De-facto way to get the length (number of elements) of an array in C
+// Simple, conventional way to get the length (number of elements) of an array in C
 #define length(array) (sizeof(array)/sizeof(array[0]))
 
 // Ask user for a monetary (float) value and return it as (integer) cents
@@ -28,6 +28,7 @@ int main(void)
 {
     // Available coin values. MUST be sorted in DESCENDING value order!
     int coins[] = {25, 10, 5, 1};
+    int len = length(coins);
 
     // Change owed, supplied by user, in cents
     int cents = get_cents();
@@ -35,17 +36,19 @@ int main(void)
     // Number of coins to be given as change
     int total = 0;
 
-    // "foreach" loop in C, adapted from
-    // https://en.wikipedia.org/wiki/Foreach_loop#C
-    for (int *c = coins, coin = *c, i = 0; c < (coins + length(coins)); coin = *(++c), i = 0)
+    // Loop each coin in coins array. <coin> is a pointer, so use as *coin
+    int *coin = coins;
+    while (len--)
     {
-        while (cents >= coin)
+        int i = 0;
+        while (cents >= *coin)
         {
-            cents -= coin;
+            cents -= *coin;
             total++;
             i++;
         }
-        fprintf(stderr, "%2i x $0.%02i\n", i, coin);
+        fprintf(stderr, "%2i x $%0.2f\n", i, *coin/100.0);
+        coin++;
     }
     printf("%i\n", total);
 
