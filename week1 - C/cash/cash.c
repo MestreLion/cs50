488022d69994b0f207c849d47af1ebbe46fd5a40
MestreLion <linux@rodrigosilva.com>
2020-11-20 16:28:12 -0300

    improve cash: comments, debugging, var names

--- week1 - C/cash/cash.c
+++ week1 - C/cash/cash.c
@@ -6,7 +6,7 @@
 #include <cs50.h>
 
 // De-facto way to get the length (number of elements) of an array in C
-#define alength(arr) (sizeof(arr)/sizeof(arr[0]))
+#define length(array) (sizeof(array)/sizeof(array[0]))
 
 // Ask user for a monetary (float) value and return it as (integer) cents
 int get_cents(void)
@@ -29,22 +29,31 @@ int main(void)
     // Available coin values. MUST be sorted in DESCENDING value order!
     int coins[] = {25, 10, 5, 1};
 
+    // Change owed, supplied by user, in cents
     int cents = get_cents();
 
+    // Number of coins to be given as change
     int total = 0;
 
     // "foreach" loop in C, adapted from
     // https://en.wikipedia.org/wiki/Foreach_loop#C
-    for (int *c = coins, coin = *c; c < (coins + alength(coins)); coin = *(++c))
+    for (int *c = coins, coin = *c, i = 0; c < (coins + length(coins)); coin = *(++c), i = 0)
     {
-        fprintf(stderr, "$0.%02i: ", coin);
         while (cents >= coin)
         {
             cents -= coin;
             total++;
-            printf(".");
+            i++;
         }
-        fprintf(stderr, "\n");
+        fprintf(stderr, "%2i x $0.%02i\n", i, coin);
     }
     printf("%i\n", total);
+
+    // Sanity check. If this ever happens, the coins list is broken
+    // (ie, the country's currency system is poorly designed)
+    if (cents)
+    {
+        fprintf(stderr, "Change left: %i cents\n", cents);
+        return 1;
+    }
 }
