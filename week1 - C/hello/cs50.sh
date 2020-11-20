57fee7fc78554c7db36e3c414091e10fda41bcb6
MestreLion <linux@rodrigosilva.com>
2020-11-20 03:19:38 -0300

    add cs50 validator/submitter script

---
+++ week1 - C/hello/cs50.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -eux
+echo "Create and use a Github Personal Access Token, do NOT use your password!"
+check50 cs50/problems/2020/x/hello
+style50 hello.c
+submit50 cs50/problems/2020/x/hello
