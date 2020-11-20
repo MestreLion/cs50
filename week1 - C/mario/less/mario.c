d25080ac788dc733d9299b43e338665974a59c7b
MestreLion <linux@rodrigosilva.com>
2020-11-20 06:20:26 -0300

    mario/less: first working version, pretty dumb

--- week1 - C/mario/less/mario.c
+++ week1 - C/mario/less/mario.c
@@ -5,4 +5,18 @@
 
 int main(void)
 {
+    int height;
+
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (!(height >= 1 && height <= 8));
+
+    for (int h = height; h; h--)
+    {
+        for (int i = 0; i < (h - 1); i++, printf("%c", ' '));
+        for (int i = 0; i < (height - h + 1); i++, printf("%c", '#'));
+        printf("\n");
+    }
 }
