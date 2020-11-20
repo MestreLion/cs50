de3170b509d26c230e29d9149601945ae166499c
MestreLion <linux@rodrigosilva.com>
2020-11-20 03:30:06 -0300

    improve cs50 validator

--- week1 - C/hello/cs50.sh
+++ week1 - C/hello/cs50.sh
@@ -1,5 +1,6 @@
-#!/bin/bash -eux
-echo "Create and use a Github Personal Access Token, do NOT use your password!"
-check50 cs50/problems/2020/x/hello
+#!/bin/bash -eu
+echo "Do NOT use your password! If prompted for one, create and use a Github Personal Access Token"
+set -x
+check50 --local cs50/problems/2020/x/hello
 style50 hello.c
 submit50 cs50/problems/2020/x/hello
