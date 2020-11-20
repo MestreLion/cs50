ca6f8dbb8480b3f19d567e62d118631453a86031
MestreLion <linux@rodrigosilva.com>
2020-11-20 05:02:25 -0300

    more comments to hello, make style50 happy

--- week1 - C/hello/hello.c
+++ week1 - C/hello/hello.c
@@ -1,14 +1,16 @@
+// https://cs50.harvard.edu/x/2020/psets/1/hello/
+
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
     string name = "";
-    // checking for empty name is not stricly needed
     do
     {
         name = get_string("What is your name? ");
     }
+    // checking for empty name is not stricly needed
     while (name[0] == '\0');
 
     printf("Hello, %s!\n", name);
