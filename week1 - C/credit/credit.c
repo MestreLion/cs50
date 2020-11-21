a98458b1c278a43fa847348537bf66ffbae9b76c
MestreLion <linux@rodrigosilva.com>
2020-11-20 21:09:12 -0300

    add credit assignment stub

---
+++ week1 - C/credit/credit.c
@@ -0,0 +1,43 @@
+// https://cs50.harvard.edu/x/2020/psets/1/credit
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+	// Credit card number, from user input
+	long cc = get_long("Number: ");
+
+	// First 2 digits (most significant, as in a string)
+	int d1 = 0, d2 = 0;
+
+	// Length of CC number
+	int length = 0;
+
+	// Current last digit
+	int d;
+
+	while(cc)
+	{
+		// Get last digit
+		d = cc % 10;
+
+		// Update first digits and length
+		d2 = d1;
+		d1 = d;
+		length++;
+
+		// "Shift" the CC number to the right, discarding last digit
+		cc -= d;
+		cc /= 10;
+	}
+	fprintf(stderr, "Length: %i\n", length);
+	fprintf(stderr, "First digits: %i%i\n", d1, d2);
+
+	if (length > 16)
+	{
+		fprintf(stderr, "Length > 16: %i\n", length);
+		printf("INVALID\n");
+		return 1;
+	}
+}
