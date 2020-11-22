a17a823c17e5515f36c6157ad0a47732f9223dbb
MestreLion <linux@rodrigosilva.com>
2020-11-22 03:51:18 -0300

    filter/less: implement grayscale

--- week4 - Memory/filter/less/helpers.c
+++ week4 - Memory/filter/less/helpers.c
@@ -1,9 +1,28 @@
 #include "helpers.h"
 
+
+// A replacement for <math.h>'s round() that relies on being coerced to integer
+// on assignment, as is the case with image pixel BYTE components.
+double round(double n)
+{
+    return (n + 0.5);
+}
+
+
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            RGBTRIPLE *p = &image[y][x];
+            BYTE c = round((p->rgbtRed + p->rgbtGreen + p->rgbtBlue) / 3.0);
+            p->rgbtRed = c;
+            p->rgbtGreen = c;
+            p->rgbtBlue = c;
+        }
+    }
 }
 
 // Convert image to sepia
