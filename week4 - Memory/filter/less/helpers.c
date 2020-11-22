7cad0bfbbd4579df3ccdc62f375638a62a2ab94f
MestreLion <linux@rodrigosilva.com>
2020-11-22 04:18:15 -0300

    filter/less: implement sepia()

--- week4 - Memory/filter/less/helpers.c
+++ week4 - Memory/filter/less/helpers.c
@@ -1,11 +1,13 @@
 #include "helpers.h"
 
 
-// A replacement for <math.h>'s round() that relies on being coerced to integer
-// on assignment, as is the case with image pixel BYTE components.
+// A replacement for <math.h>'s round() for BYTE values, as the case for pixel
+// color components. Results are capped to 255. It relies on the returned value
+// being assigned to a BYTE variable by the caller!
 double round(double n)
 {
-    return (n + 0.5);
+    n += 0.5;
+    return (n > 255) ? 255 : n;
 }
 
 
@@ -28,7 +30,18 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            // Read from copy, write to pointer, so each color read original values
+            RGBTRIPLE  c =  image[y][x];
+            RGBTRIPLE *p = &image[y][x];
+            p->rgbtRed   = round(.393 * c.rgbtRed + .769 * c.rgbtGreen + .189 * c.rgbtBlue);
+            p->rgbtGreen = round(.349 * c.rgbtRed + .686 * c.rgbtGreen + .168 * c.rgbtBlue);
+            p->rgbtBlue  = round(.272 * c.rgbtRed + .534 * c.rgbtGreen + .131 * c.rgbtBlue);
+        }
+    }
 }
 
 // Reflect image horizontally
