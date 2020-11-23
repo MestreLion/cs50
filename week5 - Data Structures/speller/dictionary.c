f13f9c60cec2aa0111f79235d0e5fd2aac4a490f
MestreLion <linux@rodrigosilva.com>
2020-11-23 06:24:29 -0300

    speller: implement hash(), load() and size()

--- week5 - Data Structures/speller/dictionary.c
+++ week5 - Data Structures/speller/dictionary.c
@@ -1,6 +1,10 @@
 // Implements a dictionary's functionality
 
+#include <errno.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "dictionary.h"
 
@@ -13,11 +17,15 @@ typedef struct node
 node;
 
 // Number of buckets in hash table
-// Initially 26, later 387.420.489 (see hash())
-const unsigned int N = 1;
+// TODO: ~143K is enough for no collision, given an uniform hash()
+const unsigned int N = 26;
 
 // Hash table
 node *table[N];
+
+// Word counter, set by load() and read by size()
+unsigned int num_words = 0;
+
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
@@ -35,40 +43,70 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO
-    // initially, (1st letter - 'a') [26 buckets]
-    // later, 27^6 buckets: first 6 letters (or all letters mod 6)
-    //   6 is largest exponent to 27 [a-z'] that fits a 4-byte int = 387.420.489
-    //   That's just 1 order of magnitude below uint range,
-    //   so it's a pretty decent key coverage
-    return 0;
+    // TODO: Optimize + KISS: Sum all chars weighed by position
+    return word[0];
 }
 
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
-    // TODO
-    // open dictionary file [on error return false]
-    // loop data (words) that are separated with '\n' [on error return false]
-    // for each word:
-    //   create a new node (malloc'ing memory to it - [on error return false])
-    //   calculate hash of word
-    //   store it in a list of hash array, at the appropriate index (from hash)
-    //     as dictionary is sorted, make sure new items placed at list head,
-    //     so lists are also sorted (in descending order). This is crucial to
-    //     optimize check()
-    //   update a global counter of words loaded. Huge optimization for size()!
-    // close file
-    // return true
-    return false;
+    // Open the dictionary file
+    FILE *file = fopen(dictionary, "rb");
+    if (file == NULL)
+    {
+        fprintf(stderr, "%s: %s\n", strerror(errno), dictionary);
+        return false;
+    }
+
+    // Craft the format string for fscanf()
+    // To avoid buffer overflows, format should not be "%s" but "%<LENGTH>s"
+    // Problem is, no such thing as fscanf("%*s", LENGTH, ...) as in printf()
+    // Solution: craft format string inserting LENGTH's value. BUT... the length
+    // of the formatting string *itself* also depends on LENGTH. Most precisly,
+    // on 2+log(LENGTH). Using (2+x/100) as a quick-and-dirty version of log(x)
+    char format[2 + (2 + LENGTH / 100) + 1];
+    sprintf(format, "%%%is", LENGTH);
+
+    // Loop words in dictionary
+    char word[LENGTH + 1];
+    while (fscanf(file, format, word) != EOF)
+    {
+        // Create a new node for word
+        node *n = malloc(sizeof(node));
+        if (!n)
+        {
+            fprintf(stderr, "Error reading word '%s': %s\n", word, strerror(errno));
+            return false;
+        }
+        // No need for strncpy(), as fscanf() already limited length
+        strcpy(n->word, word);
+
+        // Calculate hash of word to be used as index in hash table
+        unsigned int index = hash(word) % N;
+
+        // Store node, as head of its bucket
+        n->next = table[index];
+        table[index] = n;
+
+        // Update word count, a *huge* optimization for size()!
+        num_words++;
+    }
+    // Check if error when reading file data
+    if (ferror(file))
+    {
+        fprintf(stderr, "%s: %s\n", strerror(errno), dictionary);
+        fclose(file);
+        return false;
+    }
+    fclose(file);
+
+    return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    // just check the global counter
-    return 0;
+    return num_words;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
